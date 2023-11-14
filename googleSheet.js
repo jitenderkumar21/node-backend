37a0e91954383d050cad56880b80e17ba9c169ec
Jitender Kumar <70776554+jitenderkumar21@users.noreply.github.com>
2023-11-14 13:57:27 +0530

    added extra field

--- googleSheet.js
+++ googleSheet.js
@@ -8,7 +8,7 @@ const googleSheets = async (personDetails) => {
     console.log(personDetails);
     // const values = [personDetails.parentName, personDetails.childName,personDetails.email, personDetails.childAge,personDetails.classDetails[0].timeslot,personDetails.classDetails[1].timeslot,personDetails.classDetails[2].timeslot];
     
-    const values = [personDetails.parentName, personDetails.childName, personDetails.email, personDetails.childAge,personDetails.phoneNumber];
+    const values = [personDetails.parentName, personDetails.childName, personDetails.email, personDetails.childAge,personDetails.phoneNumber,personDetails.knowabout];
 
     personDetails.classDetails.forEach((classDetail) => {
         // const { classid, timeslot } = classDetail;
