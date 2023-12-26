d2fd5c1c95dc703126206a3b7d625609faa68868
Jitender Kumar <70776554+jitenderkumar21@users.noreply.github.com>
2023-12-26 10:51:57 +0530

    bug fix

--- teacherCalendar.js
+++ teacherCalendar.js
@@ -95,7 +95,6 @@ try{
                 let timeslot = classDetail.timeslot;
                 if(timeslot!=undefined && !(timeslot.toLowerCase().startsWith(prefix))){
                     if(inviteClassInfo[3]!=undefined && inviteClassInfo[4]!=undefined){
-                        sendEmailToTeacher([...inviteClassInfo,displayClassTime]);
                         // const userStartDateTime = '2023-11-19 17:00';  // Replace this with the user's input
                         // const userEndDateTime = '2023-11-19 18:00';    // Replace this with the user's input
                         const userStartDateTime =inviteClassInfo[3];  // Replace this with the user's input
@@ -114,6 +113,7 @@ try{
                             displayClassTime = `${formattedClassStartTime} - ${formattedClassEndTime} (${timeZoneAbbreviation})`;
                           }
 
+                        sendEmailToTeacher([...inviteClassInfo,displayClassTime]);
                         const startDateTime = convertToDateTimeFormat(userStartDateTime);
                         const endDateTime = convertToDateTimeFormat(userEndDateTime);
                         
