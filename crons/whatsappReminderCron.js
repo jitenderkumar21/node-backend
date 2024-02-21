4525cabaa88c6bac1f4068281ff32fbae3621b64
Jitender Kumar <70776554+jitenderkumar21@users.noreply.github.com>
2024-02-21 13:57:09 +0530

    removed log

--- crons/whatsappReminderCron.js
+++ crons/whatsappReminderCron.js
@@ -177,7 +177,7 @@ const whatsappReminderCron = cron.schedule('*/15 * * * *', async () => {
         for (const row of result.rows) {
             const reminderId = row.id;
             const additionalInfo = row.additional_info;
-            console.log(reminderId, additionalInfo)
+            // console.log(reminderId, additionalInfo)
             // await sendReminder(reminderId,row.reminder_type, additionalInfo);
         }
     } catch (error) {
