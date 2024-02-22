3bafa01f33d4fdbafc6d191fff0b79f4c3ebfa54
Jitender Kumar <70776554+jitenderkumar21@users.noreply.github.com>
2024-02-22 14:00:43 +0530

    stopped teacher email reminders

--- teacherCalendar.js
+++ teacherCalendar.js
@@ -111,7 +111,7 @@ try{
                     const modifiedClassName = ClassUtility.getModifiedClassName(subClassId,className,classTag);
                     // console.log('Modified class name',modifiedClassName);
                     if(classInviteId==undefined){
-                        createTeacherReminder(subClassId,modifiedClassName,inviteClassInfo,classIdTimings);                       
+                        // createTeacherReminder(subClassId,modifiedClassName,inviteClassInfo,classIdTimings);                       
                         const userStartDateTime =classIdTimings.get(subClassId)[0];  // Replace this with the user's input
                         const userEndDateTime = classIdTimings.get(subClassId)[1];    // Replace this with the user's input
                         const convertToDateTimeFormat = (userDateTime) => {
