19349596e18c4e9a729ab7a34657277b917f40b6
Jitender Kumar <70776554+jitenderkumar21@users.noreply.github.com>
2024-04-16 14:17:04 +0530

    waitlist in new tab

--- sheets/saveEnrollments.js
+++ sheets/saveEnrollments.js
@@ -37,7 +37,7 @@ const saveEnrollments = async (personDetails,ipAddress) => {
       ];
 
       return timeslots
-      .filter((timeslot) => !timeslot.isPast)  // Filter out timeslots where isPast is true
+      .filter((timeslot) => !timeslot.isPast && !isWaitlist)  // Filter out timeslots where isPast is true
       .map((timeslot) => {
         const { subClassId, timing, isPast, isWaitlist } = timeslot;
         let isTimeSlotWaitlist = '';
@@ -70,8 +70,65 @@ const saveEnrollments = async (personDetails,ipAddress) => {
           ipInfo.city,
           moment.tz([2023, 0], ipInfo.timezone).zoneAbbr(),
           subClassInfo.subClassName,
-          ipInfo.postal,
-          isTimeSlotWaitlist
+          ipInfo.postal
+        ];
+    
+        return values;
+      });
+    });
+
+    const walitlistRows = personDetails.classDetails.flatMap((classDetail) => {
+      const timeslots = classDetail.timeslots || [];
+      const { classid } = classDetail;
+      const inviteClassInfo = invitesInfo[classid];
+      let classStartTime, classEndTime;
+
+      if (inviteClassInfo[3] !== undefined && inviteClassInfo[4] !== undefined) {
+        // PST timings
+        classStartTime = moment(inviteClassInfo[3], 'YYYY-MM-DD HH:mm').subtract(7, 'hours');
+        classEndTime = moment(inviteClassInfo[4], 'YYYY-MM-DD HH:mm').subtract(7, 'hours');
+      }
+      const dateDayTimeColumns = [
+        classStartTime ? classStartTime.format('dddd') : '',
+        classStartTime ? classStartTime.format('h:mm A') : '',
+        classEndTime ? classEndTime.format('h:mm A') : '',
+      ];
+
+      return timeslots
+      .filter((timeslot) => !timeslot.isPast && isWaitlist)  // Filter out timeslots where isPast is true
+      .map((timeslot) => {
+        const { subClassId, timing, isPast, isWaitlist } = timeslot;
+        let isTimeSlotWaitlist = '';
+        if(isWaitlist){
+          isTimeSlotWaitlist = 'WAITLIST';
+        }
+        classIdArray.push(subClassId);
+        let subClassInfo = subClassesInfo[subClassId];
+        const classIdFomatted = ClassUtility.getClassId(subClassId, classDetail.classTag);
+        const values = [
+          formattedTimestamp,
+          personDetails.parentName,
+          personDetails.childName,
+          personDetails.email,
+          personDetails.childAge,
+          personDetails.commPref.join(','),
+          personDetails.phoneNumber,
+          subClassInfo.teacherName,
+          personDetails.knowabout,
+          personDetails.additionalInfo,
+          personDetails.comments,
+          subClassId,
+          classDetail.classTag,
+          classDetail.className,
+          timing.split(':')[1].trim(),
+          ...dateDayTimeColumns,
+          '',
+          ipInfo.country,
+          ipInfo.region,
+          ipInfo.city,
+          moment.tz([2023, 0], ipInfo.timezone).zoneAbbr(),
+          subClassInfo.subClassName,
+          ipInfo.postal
         ];
     
         return values;
@@ -133,6 +190,17 @@ const saveEnrollments = async (personDetails,ipAddress) => {
         values: rows,
       },
     });
+
+    google.sheets({ version: 'v4', auth: client }).spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range: 'Waitlist!A:P', // Adjust the range as needed
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values: walitlistRows,
+      },
+    });
+
     console.log('Enrollments Saved in Format 1 successfully');
     bulkInsertEnrollments(rows);
     const reportData = { channel: 'SHEETS', type: 'Save Enrollments', status: 'SUCCESS', parentEmail: personDetails.email, childName: personDetails.childName, classId:classIdArray};
